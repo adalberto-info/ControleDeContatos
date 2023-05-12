af4925880521d149228f4466b92a94ea9ded09ae
adalberto-info <illuminati.spiritus@gmail.com>
2023-05-11 23:20:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ControleDeContatos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var Configuration = builder.Configuration;
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+            builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+
+        public IConfigurationRoot? Configuration { get; }
+    }
+}
---
+++ Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel Atualizar(ContatoModel contato);
+
+        bool Apagar(int id);
+
+    }
+}
