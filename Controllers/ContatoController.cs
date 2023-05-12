06371b457d9e7cecdb7f63afc6a9ec9bda0562bb
adalberto-info <illuminati.spiritus@gmail.com>
2023-05-12 01:43:38 -0300

    atualizacao do projeto

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -40,24 +40,72 @@ namespace ControleDeContatos.Controllers
 
         public IActionResult Apagar(int id)
         {
-            _contatoRepositorio.Apagar(id);
-            return RedirectToAction("Index");
+            try
+            {
+                bool apagado = _contatoRepositorio.Apagar(id);
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Contato excluído com sucesso!";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, não conseguimos excluir seu contato!";
+                }
+                return RedirectToAction("Index");
+
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos excluir seu contato, tente novamente. Detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+
+            }
+
         }
 
         [HttpPost]
         public IActionResult Criar( ContatoModel contato)
         {
-            _contatoRepositorio.Adicionar(contato);
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso!";
+                    return RedirectToAction("Index");
 
-            return RedirectToAction("Index");
+                }
+                return View(contato);
+
+            }
+            catch (Exception erro) {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu contato, tente novamente. Detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
         }
 
         [HttpPost]
         public IActionResult Alterar(ContatoModel contato)
         {
-            _contatoRepositorio.Atualizar(contato);
 
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Atualizar(contato);
+                    TempData["MensagemSucesso"] = "Contato alterado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+                return View("Editar", contato);
+
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos alterar seu contato, tente novamente. Detalhe do erro:{erro.Message}";
+                return RedirectToAction("Index");
+            }
+
         }
 
 
--- Models/ContatoModel.cs
+++ Models/ContatoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +11,13 @@ namespace ControleDeContatos.Models
     public class ContatoModel
     {
         public int Id { get; set; }
-        public string? Nome { get; set; } 
-        public string? Email { get; set; } 
+        [Required(ErrorMessage = "Digite o nome do contato.")]
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Digite o e-mail do contato.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Digite o celular do contato.")]
+        [Phone(ErrorMessage = "O celular informado não é válido.")]
         public string? Celular { get; set; } 
 
 
